4e368cff87ad4988de89d5135fa2589e5bd6619f
binazgreat <binazgreat@gmail.com>
2022-10-17 10:13:46 -0700

    Fix for 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = 
+        self.__height = value
 
     def area(self):
         """Return the area of the Rectangle."""
